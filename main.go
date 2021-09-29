d3a9ca617c8a6120db89f5472cf55a051eb2c0c3
Kacper Bąk <kacper.bak@sonderfin.com>
2021-09-29 23:59:49 +0200

    feat: renamed var

--- main.go
+++ main.go
@@ -9,26 +9,26 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/employeedb")
+	db, err := sql.Open("mysql", "root:123@tcp(127.0.0.1:3306)/employeedb")
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		fmt.Println("Connection Established")
 	}
 	var (
-		id   int
-		name string
+		id       int
+		username string
 	)
 	rows, err := db.Query("select id, username from user where id = ?", 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for rows.Next() {
-		err := rows.Scan(&id, &name)
+		err := rows.Scan(&id, &username)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(id, name)
+		fmt.Println(id, username)
 	}
 	err = rows.Err()
 	if err != nil {
